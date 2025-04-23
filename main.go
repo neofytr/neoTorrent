56579008b97ce466c15346aa20f523c83b669405
neofytr <imagination12344321.com@gmail.com>
2025-04-23 22:57:05 +0530

    info about bittorrent

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+/*
+
+BitTorrent is a protocol for downloading and distributing files across the Internet.
+In contrast with the traditional client/server relationship, in which downloaders connect to the
+central server, participants in the BitTorrent network, called peers, download pieces of files from
+each other - this is what makes it a peer-to-peer protocol.
+
+*/
+
 func main() {
 	fmt.Println("hello, world!")
 }
